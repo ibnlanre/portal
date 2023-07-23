8f2ea48d6598da01d1ea7b5cefe4d59bae148838
Ridwan Olanrewaju <olaitanlanre28@gmail.com>
2023-07-23 23:11:36 +0100

    change implementation of cookie storage

--- src/addons/withCookieStorage.ts
+++ src/addons/withCookieStorage.ts
@@ -1,21 +1,17 @@
 import { useState, useEffect, type Reducer } from "react";
 
 import { cookieStorage } from "component";
-import { isCookieEntry, objectToStringKey } from "utilities";
+import { objectToStringKey } from "utilities";
 import { getCookieValue } from "cookies";
 
-import type { Initial, PortalState, CookieEntry } from "definition";
+import type { PortalState, CookieEntry } from "definition";
 
 import { usePortalImplementation } from "./withImplementation";
 
 export function usePortalWithCookieStorage<
-  S extends string | CookieEntry,
+  S extends CookieEntry,
   A = undefined
->(
-  key: any,
-  initialState?: Initial<S>,
-  reducer?: Reducer<S, A>
-): PortalState<S, A> {
+>(key: any, initialState: S, reducer?: Reducer<S, A>): PortalState<S, A> {
   const stringKey = objectToStringKey(key);
   let overrideApplicationState = false;
 
@@ -23,7 +19,7 @@ export function usePortalWithCookieStorage<
     const value = getCookieValue(stringKey);
     if (value !== null) {
       overrideApplicationState = true;
-      return value as S;
+      return { ...initialState, value };
     }
     return initialState;
   });
@@ -36,12 +32,8 @@ export function usePortalWithCookieStorage<
   });
 
   useEffect(() => {
-    if (typeof state === "string") cookieStorage.setItem(stringKey, state);
-    else if (isCookieEntry(state)) {
-      const { value, ...options } = { ...state };
-      const prevValue = cookieStorage.getItem(stringKey) ?? "";
-      cookieStorage.setItem(stringKey, value ?? prevValue, options);
-    }
+    const { value, ...options } = state;
+    cookieStorage.setItem(stringKey, state.value, options);
   }, [state]);
 
   return [state, setState];
--- src/utilities/isCookieEntry.ts
+++ src/utilities/isCookieEntry.ts
@@ -6,6 +6,6 @@ import { CookieEntry } from "definition";
  * @param {CookieEntry} value The value to be checked.
  * @returns {value is CookieEntry} `true` if the entry has a `value` property, `false` otherwise.
  */
-export function isCookieEntry(entry: CookieEntry): entry is CookieEntry {
+export function isCookieEntry<S>(entry: S | CookieEntry): entry is CookieEntry {
   return entry && typeof entry === "object" && "value" in entry;
 }
