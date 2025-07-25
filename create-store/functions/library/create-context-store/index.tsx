c689d104e40fd39a6f2718b45f8e24bb635d8d51
Ridwan Olanrewaju <olaitanlanre28@gmail.com>
2025-07-25 23:06:49 +0100

    refactor: enhance createContextStore by allowing custom display names and improving error handling

--- create-store/functions/library/create-context-store/index.tsx
+++ create-store/functions/library/create-context-store/index.tsx
@@ -3,19 +3,23 @@ import type { PropsWithChildren } from "react";
 import type { ContextStore } from "@/create-store/types/context-store";
 import type { ContextValue } from "@/create-store/types/context-value";
 
-import { createContext, useContext, useMemo } from "react";
+import { createContext, useContext } from "react";
 
 /**
  * Initializes a context store with a provider and a hook for accessing its value.
  *
  * @param use A hook function that generates the store from the given context.
+ * @param displayName An optional display name for the context, used for debugging.
+ *                    Defaults to "StoreContext" if not provided.
+ *
  * @returns A tuple with the StoreProvider component and the useStore hook.
  */
 export function createContextStore<Context, Store>(
-  use: ContextValue<Context, Store>
+  use: ContextValue<Context, Store>,
+  displayName: string = "StoreContext"
 ): ContextStore<Context, Store> {
   const StoreContext = createContext<null | Store>(null);
-  StoreContext.displayName = "StoreContext";
+  StoreContext.displayName = displayName;
 
   function StoreProvider({
     children,
@@ -27,11 +31,13 @@ export function createContextStore<Context, Store>(
     );
   }
 
-  function useStore() {
+  function useStore(): Store {
     const store = useContext(StoreContext);
 
-    if (!store) {
-      throw new Error("The store hook must be used within its provider.");
+    if (store === null) {
+      throw new Error(
+        `Make sure your component is wrapped in <${displayName}.Provider>`
+      );
     }
 
     return store;
